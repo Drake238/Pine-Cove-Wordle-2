3a6af16ac792a07f54e2805cbf8386cbc154473d
Drake Carson <carsondrake238@gmail.com>
2022-02-14 09:41:04 -0600

    revert back to localhost

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const messageDisplay = document.querySelector('.message-container')
 let wordle
 
 const getWordle = () => {
-    fetch('https://drake238.github.io/Pine-Cove-Wordle-2/')
+    fetch('http://localhost:8000/word')
         .then(response => response.json())
         .then(json => {
             console.log(json)
@@ -124,7 +124,7 @@ const checkRow = () => {
     const guess = guessRows[currentRow].join('')
     console.log('guess', guess)
     if (currentTile > 4) {
-        fetch(`https://drake238.github.io/Pine-Cove-Wordle-2/${guess}`)
+        fetch(`http://localhost:8000/word${guess}`)
             .then(response => response.json())
             .then(json => {
                 console.log(json)
